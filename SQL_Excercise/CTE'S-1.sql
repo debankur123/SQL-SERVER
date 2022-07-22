c108289f5f604255f5e0b892598b6470dd317dce
Debankur-embee <debankur.b@embee.co.in>
2022-07-22 20:41:55 +0530

    CTE-commit

--- SQL_Excercise/CTE'S-1.sql
+++ SQL_Excercise/CTE'S-1.sql
@@ -2,6 +2,9 @@
     The Common Table Expressions (CTE) were introduced into standard SQL in order to simplify
      various classes of SQL Queries for which a derived table was just unsuitable.In  Simple SQL
      words to get reduced from complex sub-queries it is used. 
+	 the common table expression (CTE) is a temporary named result set that you can reference within a 
+	 SELECT, INSERT, UPDATE, or DELETE statement. 
+	 You can also use a CTE in a CREATE a view, as part of the view’s SELECT query.
 
      The problem for CTE is as given below:
 
@@ -44,3 +47,25 @@ left  join
     group by OrderMonth
 ) B on A.OrderMonth = dateadd(MONTH,1, B.OrderMonth)
 order by 1
+--------------------------------------------------------------------------------------------
+/*
+	Now we will do this complicated sub-query using CTE'S
+*/
+with Sales1 as(                      -- starts with 'WITH' clause .
+	select 
+		OrderDate,
+		TotalDue,
+		OrderMonth = DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1),
+		OrderRank = ROW_NUMBER() over(partition by DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1) order by TotalDue desc)
+		from Sales.SalesOrderHeader 
+),
+Top10 as (
+	select
+	OrderMonth,
+	Top10Total = SUM(TotalDue)
+	from Sales1
+	where OrderRank<10
+	group by OrderMonth
+)
+
+select * from Top10
